0a300cefc752fda5d032349c2d2891f0b451e609
BauwkeSpooren <r0831607@student.thomasmore.be>
2021-02-16 22:21:25 +0100

    overal navbar inzetten & venuelist maken

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -44,4 +44,9 @@ public class HomeController {
         model.addAttribute("venueName",venueName);
         return "venuedetails";
     }
+
+    @GetMapping("/venuelist")
+    public String venuelist(Model model) {
+        return "venuelist";
+    }
 }
