2d7c39ebd8ec1c562412648e051b689d651e3596
BauwkeSpooren <r0831607@student.thomasmore.be>
2021-02-23 16:29:04 +0100

    venues java class gemaakt; venuedetails compleet

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Venue;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,14 @@ import java.util.Date;
 
 public class HomeController {
 
-    private final String appName = "WOOOO";
-    private final String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-    private final String paydate = LocalDate.now().plusDays(30).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-    private final String[] venueNames = {"De Loods", "De Club", "De Hanger", "Zapoi", "Kuub", "Cuba Libre"};
+    private final Venue[] venues = {
+            new Venue("De Loods","https://www.facebook.com/boesjkammeree/", 5,true, false, false, true,"france",5000),
+            new Venue("De Club","https://www.youtube.com/watch?v=4rlc2EgSdbU&list=PL0bHKk6wuUGLWGipKSf0dFrpuzDitERqD", 5,true, true, false, false,"dsfdsfds",555555),
+            new Venue("De Hanger","https://www.facebook.com/boesjkammeree/", 5,false, true, false, true,"iolouiouio",2555)
+    };
 
     @GetMapping({"/","/home"})
     public String home(Model model) {
-        model.addAttribute("appName", appName);
         return "home";
     }
 
@@ -31,28 +32,21 @@ public class HomeController {
         return "about";
     }
 
-    @GetMapping("/pay")
-    public String pay(Model model) {
-        model.addAttribute("date",date);
-        model.addAttribute("paydate",paydate);
-        model.addAttribute("localDateTime", LocalDateTime.now());
-        return "pay";
-    }
-
     @GetMapping({"/venuedetails", "/venuedetails/{index}"})
     public String venueDetails(Model model,
                                @PathVariable(required = false)  Integer index) {
-        if (index!=null && index>=0 && index<venueNames.length ) {
-            model.addAttribute("venueName", venueNames[index]);
-            model.addAttribute("prevIndex", index>0 ? index-1 : venueNames.length-1);
-            model.addAttribute("nextIndex", index<venueNames.length-1 ? index+1 : 0);
+        if (index!=null && index>=0 && index<venues.length ) {
+            model.addAttribute("venueName", venues[index]);
+            model.addAttribute("prevIndex", index>0 ? index-1 : venues.length-1);
+            model.addAttribute("nextIndex", index<venues.length-1 ? index+1 : 0);
+            model.addAttribute("venues", venues);
         }
         return "venuedetails";
     }
 
     @GetMapping("/venuelist")
     public String venuelist(Model model) {
-        model.addAttribute("venueNames", venueNames);
+        model.addAttribute("venues", venues);
         return "venuelist";
     }
 }
